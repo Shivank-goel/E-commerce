2182e75cb5dbb39cdab6e9406ed88e1d8b85d18f
hivank Goel <shivankgoel2@gmail.com>
2023-08-13 17:25:39 +0530

    nav and carousel

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation'
+import HomePage from './customer/pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation/>
+
+    <div><HomePage/></div>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from "../../HomeCarousel/MainCarousel"
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCarousel/>
+
+            <div>
+                other section
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
